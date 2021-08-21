59e71d5eafa32610789519b7621438f619063fc7
Shubham Kumar <hello@shubhkumar.in>
2021-08-21 19:42:15 +0530

    Updated CI  for release.

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const express = require('express')
-const morgan = require('morgan')
-const compression = require('compression')
 const fs = require('fs')
 const jsdom = require('jsdom')
 const useEnv = require('./env')
@@ -18,14 +15,3 @@ fs.readFile('./template/index.html', 'utf8', (err, file) => {
     }
   });
 });
-
-const app = express();
-app.use(morgan('combined'));
-app.use(express.static('www'));
-app.use(compression())
-
-const server = app.listen(process.env.PORT || 3000, () => {
-    const host = server.address().address
-    const port = server.address().port
-    console.log('Express app listening at http://%s:%s', host, port)
-})
