50603f8c3151d558e6f92143062db0678c9e34f5
Shubham Kumar <hello@shubhkumar.in>
2021-08-22 14:20:43 +0530

    Changed process.env.change.

--- env.js
+++ env.js
@@ -1,89 +1,90 @@
 module.exports = useEnv;
 
 var env = {
-    META_TITLE: '$META_TITLE',
-    META_DESCRIPTION: '$META_DESCRIPTION',
-    META_AUTHOR: '$META_AUTHOR',
-    THEME: '$THEME',
-    FAVICON_URL: '$FAVICON_URL',
-    AVATAR_URL: '$AVATAR_URL',
-    AVATAR_ALT: '$AVATAR_ALT',
-    AVATAR_2X_URL : '$AVATAR_2X_URL',
-    NAME:'$NAME',
-    BIO: '$BIO',
-    GITHUB: '$GITHUB',
-    TWITTER: '$TWITTER',
-    MASTODON: '$MASTODON',
-    MICRO_BLOG: '$MICRO_BLOG',
-    INSTAGRAM: '$INSTAGRAM',
-    FACEBOOK: '$FACEBOOK',
-    FACEBOOK_MESSENGER: '$FACEBOOK_MESSENGER',
-    LINKED_IN: '$LINKED_IN',
-    YOUTUBE: '$YOUTUBE',
-    DISCORD: '$DISCORD',
-    TWITCH: '$TWITCH',
-    PRODUCT_HUNT: '$PRODUCT_HUNT',
-    SNAPCHAT: '$SNAPCHAT',
-    SPOTIFY: '$SPOTIFY',
-    REDDIT: '$REDDIT',
-    MEDIUM: '$MEDIUM',
-    PINTEREST: '$PINTEREST',
-    TIKTOK: '$TIKTOK',
-    EMAIL: '$EMAIL',
-    EMAIL_TEXT: '$EMAIL_TEXT',
-    EMAIL_ALT: '$EMAIL_ALT',
-    EMAIL_ALT_TEXT: '$EMAIL_ALT_TEXT',
-    SOUND_CLOUD: '$SOUND_CLOUD',
-    FIGMA: '$FIGMA',
-    KIT: '$KIT',
-    TELEGRAM: '$TELEGRAM',
-    TUMBLR: '$TUMBLR',
-    STEAM: '$STEAM',
-    VIMEO: '$VIMEO',
-    WORDPRESS: '$WORDPRESS',
-    GOODREADS: '$GOODREADS',
-    SKOOB: '$SKOOB',
-    LETTERBOXD: '$LETTERBOXD',
-    FOOTER: '$FOOTER',
+    META_TITLE: process.env.META_TITLE,
+    META_DESCRIPTION: process.env.META_DESCRIPTION,
+    META_AUTHOR: process.env.META_AUTHOR,
+    THEME: process.env.THEME,
+    FAVICON_URL: process.env.FAVICON_URL,
+    AVATAR_URL: process.env.AVATAR_URL,
+    AVATAR_ALT: process.env.AVATAR_ALT,
+    AVATAR_2X_URL: process.env.AVATAR_2X_URL,
+    NAME: process.env.NAME,
+    BIO: process.env.BIO,
+    GITHUB: process.env.GITHUB,
+    TWITTER: process.env.TWITTER,
+    MASTODON: process.env.MASTODON,
+    MICRO_BLOG: process.env.MICRO_BLOG,
+    INSTAGRAM: process.env.INSTAGRAM,
+    FACEBOOK: process.env.FACEBOOK,
+    FACEBOOK_MESSENGER: process.env.FACEBOOK_MESSENGER,
+    LINKED_IN: process.env.LINKED_IN,
+    YOUTUBE: process.env.YOUTUBE,
+    DISCORD: process.env.DISCORD,
+    TWITCH: process.env.TWITCH,
+    PRODUCT_HUNT: process.env.PRODUCT_HUNT,
+    SNAPCHAT: process.env.SNAPCHAT,
+    SPOTIFY: process.env.SPOTIFY,
+    REDDIT: process.env.REDDIT,
+    MEDIUM: process.env.MEDIUM,
+    PINTEREST: process.env.PINTEREST,
+    TIKTOK: process.env.TIKTOK,
+    EMAIL: process.env.EMAIL,
+    EMAIL_TEXT: process.env.EMAIL_TEXT,
+    EMAIL_ALT: process.env.EMAIL_ALT,
+    EMAIL_ALT_TEXT: process.env.EMAIL_ALT_TEXT,
+    SOUND_CLOUD: process.env.SOUND_CLOUD,
+    FIGMA: process.env.FIGMA,
+    KIT: process.env.KIT,
+    TELEGRAM: process.env.TELEGRAM,
+    TUMBLR: process.env.TUMBLR,
+    STEAM: process.env.STEAM,
+    VIMEO: process.env.VIMEO,
+    WORDPRESS: process.env.WORDPRESS,
+    GOODREADS: process.env.GOODREADS,
+    SKOOB: process.env.SKOOB,
+    LETTERBOXD: process.env.LETTERBOXD,
+    FOOTER: process.env.FOOTER,
 
 }
 
 function useEnv(document) {
+    console.log(env);
     var metalTitleEl = document.getElementById('meta-title');
-    if  (env.META_TITLE) {
+    if (env.META_TITLE) {
         metalTitleEl.textContent = env.META_TITLE;
     } else {
         metalTitleEl.remove()
     }
 
     var metalDescriptionEl = document.getElementById('meta-description');
-    if  (env.META_DESCRIPTION) {
+    if (env.META_DESCRIPTION) {
         metalDescriptionEl.content = env.META_DESCRIPTION;
     } else {
         metalDescriptionEl.remove()
     }
 
     var metaAuthorEl = document.getElementById('meta-author');
-    if  (env.META_AUTHOR) {
+    if (env.META_AUTHOR) {
         metaAuthorEl.content = env.META_AUTHOR;
     } else {
         metaAuthorEl.remove()
     }
 
     var themeEl = document.getElementById('theme');
-    if  (env.THEME && env.THEME.toLocaleLowerCase() === 'dark') {
+    if (env.THEME && env.THEME.toLocaleLowerCase() === 'dark') {
         themeEl.href = 'css/skeleton-dark.css';
     } else {
         themeEl.href = 'css/skeleton-light.css';
     }
 
     var faviconEl = document.getElementById('favicon');
-    if  (env.FAVICON_URL) {
+    if (env.FAVICON_URL) {
         faviconEl.href = env.FAVICON_URL;
     }
 
     var avatarEl = document.getElementById('avatar');
-    if  (env.AVATAR_URL) {
+    if (env.AVATAR_URL) {
         avatarEl.src = env.AVATAR_URL;
         avatarEl.alt = env.AVATAR_ALT;
         avatarEl.srcset = env.AVATAR_2X_URL + ' 2x';
@@ -92,21 +93,21 @@ function useEnv(document) {
     }
 
     var nameEl = document.getElementById('name');
-    if  (env.NAME) {
-        nameEl.innerHTML  = env.NAME;
+    if (env.NAME) {
+        nameEl.innerHTML = env.NAME;
     } else {
         nameEl.remove()
     }
 
     var bioEl = document.getElementById('bio');
-    if  (env.BIO) {
-        bioEl.innerHTML  = env.BIO;
+    if (env.BIO) {
+        bioEl.innerHTML = env.BIO;
     } else {
         bioEl.remove()
     }
 
     var githubEl = document.getElementById('github');
-    if  (env.GITHUB) {
+    if (env.GITHUB) {
         githubEl.href = env.GITHUB;
     } else {
         githubEl.nextElementSibling.remove()
@@ -114,7 +115,7 @@ function useEnv(document) {
     }
 
     var twitterEl = document.getElementById('twitter');
-    if  (env.TWITTER) {
+    if (env.TWITTER) {
         twitterEl.href = env.TWITTER;
     } else {
         twitterEl.nextElementSibling.remove()
@@ -122,7 +123,7 @@ function useEnv(document) {
     }
 
     var mastodonEl = document.getElementById('mastodon');
-    if  (env.MASTODON) {
+    if (env.MASTODON) {
         mastodonEl.href = env.MASTODON;
     } else {
         mastodonEl.nextElementSibling.remove()
@@ -130,7 +131,7 @@ function useEnv(document) {
     }
 
     var microblogEl = document.getElementById('microblog');
-    if  (env.MICRO_BLOG) {
+    if (env.MICRO_BLOG) {
         microblogEl.href = env.MICRO_BLOG;
     } else {
         microblogEl.nextElementSibling.remove()
@@ -138,7 +139,7 @@ function useEnv(document) {
     }
 
     var instagramEl = document.getElementById('instagram');
-    if  (env.INSTAGRAM) {
+    if (env.INSTAGRAM) {
         instagramEl.href = env.INSTAGRAM;
     } else {
         instagramEl.nextElementSibling.remove()
@@ -146,7 +147,7 @@ function useEnv(document) {
     }
 
     var facebookEl = document.getElementById('facebook');
-    if  (env.FACEBOOK) {
+    if (env.FACEBOOK) {
         facebookEl.href = env.FACEBOOK;
     } else {
         facebookEl.nextElementSibling.remove()
@@ -154,7 +155,7 @@ function useEnv(document) {
     }
 
     var facebookMessengerEl = document.getElementById('facebook-messenger');
-    if  (env.FACEBOOK_MESSENGER) {
+    if (env.FACEBOOK_MESSENGER) {
         facebookMessengerEl.href = env.FACEBOOK_MESSENGER;
     } else {
         facebookMessengerEl.nextElementSibling.remove()
@@ -163,7 +164,7 @@ function useEnv(document) {
 
 
     var linkedInEl = document.getElementById('linkedin');
-    if  (env.LINKED_IN) {
+    if (env.LINKED_IN) {
         linkedInEl.href = env.LINKED_IN;
     } else {
         linkedInEl.nextElementSibling.remove()
@@ -172,7 +173,7 @@ function useEnv(document) {
 
 
     var youTubeEl = document.getElementById('youtube');
-    if  (env.YOUTUBE) {
+    if (env.YOUTUBE) {
         youTubeEl.href = env.YOUTUBE;
     } else {
         youTubeEl.nextElementSibling.remove()
@@ -181,7 +182,7 @@ function useEnv(document) {
 
 
     var discordEl = document.getElementById('discord');
-    if  (env.DISCORD) {
+    if (env.DISCORD) {
         discordEl.href = env.DISCORD;
     } else {
         discordEl.nextElementSibling.remove()
@@ -190,7 +191,7 @@ function useEnv(document) {
 
 
     var twitchEl = document.getElementById('twitch');
-    if  (env.TWITCH) {
+    if (env.TWITCH) {
         twitchEl.href = env.TWITCH;
     } else {
         twitchEl.nextElementSibling.remove()
@@ -198,7 +199,7 @@ function useEnv(document) {
     }
 
     var productHunEl = document.getElementById('producthunt');
-    if  (env.PRODUCT_HUNT) {
+    if (env.PRODUCT_HUNT) {
         productHunEl.href = env.PRODUCT_HUNT;
     } else {
         productHunEl.nextElementSibling.remove()
@@ -206,7 +207,7 @@ function useEnv(document) {
     }
 
     var snapchatEl = document.getElementById('snapchat');
-    if  (env.SNAPCHAT) {
+    if (env.SNAPCHAT) {
         snapchatEl.href = env.SNAPCHAT;
     } else {
         snapchatEl.nextElementSibling.remove()
@@ -214,7 +215,7 @@ function useEnv(document) {
     }
 
     var spotifyEl = document.getElementById('spotify');
-    if  (env.SPOTIFY) {
+    if (env.SPOTIFY) {
         spotifyEl.href = env.SPOTIFY;
     } else {
         spotifyEl.nextElementSibling.remove()
@@ -222,7 +223,7 @@ function useEnv(document) {
     }
 
     var redditEl = document.getElementById('reddit');
-    if  (env.REDDIT) {
+    if (env.REDDIT) {
         redditEl.href = env.REDDIT;
     } else {
         redditEl.nextElementSibling.remove()
@@ -231,7 +232,7 @@ function useEnv(document) {
 
 
     var mediumEl = document.getElementById('medium');
-    if  (env.MEDIUM) {
+    if (env.MEDIUM) {
         mediumEl.href = env.MEDIUM;
     } else {
         mediumEl.nextElementSibling.remove()
@@ -240,7 +241,7 @@ function useEnv(document) {
 
 
     var pinterestEl = document.getElementById('pinterest');
-    if  (env.PINTEREST) {
+    if (env.PINTEREST) {
         pinterestEl.href = env.PINTEREST;
     } else {
         pinterestEl.nextElementSibling.remove()
@@ -248,7 +249,7 @@ function useEnv(document) {
     }
 
     var tiktokEl = document.getElementById('tiktok');
-    if  (env.TIKTOK) {
+    if (env.TIKTOK) {
         tiktokEl.href = env.TIKTOK;
     } else {
         tiktokEl.nextElementSibling.remove()
@@ -256,7 +257,7 @@ function useEnv(document) {
     }
 
     var emailEl = document.getElementById('email');
-    if  (env.EMAIL) {
+    if (env.EMAIL) {
         emailEl.innerHTML = env.EMAIL_TEXT || env.EMAIL
         emailEl.href = 'mailto:' + env.EMAIL;
     } else {
@@ -266,7 +267,7 @@ function useEnv(document) {
 
 
     var emailAltEl = document.getElementById('email-alt');
-    if  (env.EMAIL_ALT) {
+    if (env.EMAIL_ALT) {
         emailAltEl.innerHTML = env.EMAIL_ALT_TEXT || env.EMAIL_ALT
         emailAltEl.href = 'mailto:' + env.EMAIL_ALT;
     } else {
@@ -275,7 +276,7 @@ function useEnv(document) {
     }
 
     var soundCloudEl = document.getElementById('soundcloud');
-    if  (env.SOUND_CLOUD) {
+    if (env.SOUND_CLOUD) {
         soundCloudEl.href = env.SOUND_CLOUD;
     } else {
         soundCloudEl.nextElementSibling.remove()
@@ -284,7 +285,7 @@ function useEnv(document) {
 
 
     var figmaEl = document.getElementById('figma');
-    if  (env.FIGMA) {
+    if (env.FIGMA) {
         figmaEl.href = env.FIGMA;
     } else {
         figmaEl.nextElementSibling.remove()
@@ -293,7 +294,7 @@ function useEnv(document) {
 
 
     var kitEl = document.getElementById('kit');
-    if  (env.KIT) {
+    if (env.KIT) {
         kitEl.href = env.KIT;
     } else {
         kitEl.nextElementSibling.remove()
@@ -302,7 +303,7 @@ function useEnv(document) {
 
 
     var telegramEl = document.getElementById('telegram');
-    if  (env.TELEGRAM) {
+    if (env.TELEGRAM) {
         telegramEl.href = env.TELEGRAM;
     } else {
         telegramEl.nextElementSibling.remove()
@@ -310,7 +311,7 @@ function useEnv(document) {
     }
 
     var temblrEl = document.getElementById('tumblr');
-    if  (env.TUMBLR) {
+    if (env.TUMBLR) {
         temblrEl.href = env.TUMBLR;
     } else {
         temblrEl.nextElementSibling.remove()
@@ -319,7 +320,7 @@ function useEnv(document) {
 
 
     var steamEl = document.getElementById('steam');
-    if  (env.STEAM) {
+    if (env.STEAM) {
         steamEl.href = env.STEAM;
     } else {
         steamEl.nextElementSibling.remove()
@@ -328,7 +329,7 @@ function useEnv(document) {
 
 
     var vimeoEl = document.getElementById('vimeo');
-    if  (env.VIMEO) {
+    if (env.VIMEO) {
         vimeoEl.href = env.VIMEO;
     } else {
         vimeoEl.nextElementSibling.remove()
@@ -337,7 +338,7 @@ function useEnv(document) {
 
 
     var wordpressEl = document.getElementById('wordpress');
-    if  (env.WORDPRESS) {
+    if (env.WORDPRESS) {
         wordpressEl.href = env.WORDPRESS;
     } else {
         wordpressEl.nextElementSibling.remove()
@@ -345,7 +346,7 @@ function useEnv(document) {
     }
 
     var goodreadsEl = document.getElementById('goodreads');
-    if  (env.GOODREADS) {
+    if (env.GOODREADS) {
         goodreadsEl.href = env.GOODREADS;
     } else {
         goodreadsEl.nextElementSibling.remove()
@@ -353,7 +354,7 @@ function useEnv(document) {
     }
 
     var skoobEl = document.getElementById('skoob');
-    if  (env.SKOOB) {
+    if (env.SKOOB) {
         skoobEl.href = env.SKOOB;
     } else {
         skoobEl.nextElementSibling.remove()
@@ -369,8 +370,8 @@ function useEnv(document) {
     }
 
     var footerEl = document.getElementById('footer');
-    if  (env.FOOTER) {
-        footerEl.innerHTML  = env.FOOTER;
+    if (env.FOOTER) {
+        footerEl.innerHTML = env.FOOTER;
     } else {
         footerEl.remove()
     }
