3e1224ddba07f1b919f0ec8ac091f6d2c473d113
Max Graman <me@maxg.xyz>
2021-08-31 15:18:16 -0400

    Fixed extra p

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -304,7 +304,7 @@ function Home(props) {
               <Button
                 name="whatsapp"
                 href={runtimeConfig.WHATSAPP}
-                displayName="WhatsAppp"
+                displayName="WhatsApp"
                 logo={whatsappLogo}
               />
             )}
