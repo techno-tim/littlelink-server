5805a96e33e2d741fff36faba1f2917abc17eaaa
Choubakawa <Choubakawa@users.noreply.github.com>
2022-11-02 20:42:09 +0100

    feat(style): added shadow on buttons and avatar #191

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,12 +1,20 @@
 import React, { memo } from 'react';
 import { string } from 'prop-types';
+import { addShadow } from '../../utils';
 
 import './Avatar.css';
 
 function Avatar(props) {
   const { src, srcSet, alt } = props;
 
-  return <img className="avatar" src={src} srcSet={srcSet} alt={alt} />;
+  return (
+    <img
+      className={'avatar' + addShadow()}
+      src={src}
+      srcSet={srcSet}
+      alt={alt}
+    />
+  );
 }
 
 export default memo(Avatar);
