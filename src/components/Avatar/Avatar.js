8626012d37f6914024d087e52a19ea7192e1e3a1
Maxim Tacu <maxtacu95@gmail.com>
2023-10-31 00:56:20 +0000

    Add and configure AVATAR_SIZE environment variable

--- src/components/Avatar/Avatar.js
+++ src/components/Avatar/Avatar.js
@@ -1,13 +1,23 @@
 import React, { memo } from 'react';
 import { string } from 'prop-types';
 import { addShadow } from '../../utils';
+import { runtimeConfig } from '../../config';
 
 import './Avatar.css';
 
 function Avatar(props) {
   const { src, srcSet, alt } = props;
+  const avatarSize = runtimeConfig.AVATAR_SIZE || null;
 
-  return (
+  return avatarSize ? (
+    <img
+      className={'avatar' + addShadow()}
+      src={src}
+      srcSet={srcSet}
+      alt={alt}
+      style={{ width: avatarSize, height: avatarSize }}
+    />
+  ) : (
     <img
       className={'avatar' + addShadow()}
       src={src}
